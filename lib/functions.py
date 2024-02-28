5d988cdc4acd58a7d2c43482dbf47b0a2f4d9c7b
zeid <zeidhassan4@gmail.com>
2024-02-28 17:44:15 +0300

    Done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+greet("Zeid")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default("Harith")
 
 def add(num1, num2):
-    pass
+    sum = num1 + num2
+    print(sum)
+    return sum
+add(40, 500)    
 
 def halve(number):
-    pass
+    my_half = number/2
+    print(my_half)
+    return my_half
+halve(8)    
